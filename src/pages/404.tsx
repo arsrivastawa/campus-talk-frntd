71aa909f8e6b963aa2c880cdadd291c446036d98
arsrivastawa <arsrivastawa2710@gmail.com>
2025-07-20 15:30:39 +0530

    Added: 404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,4 @@
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
-
 const Error404 = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.error(
-      "404 Error: User attempted to access non-existent route:",
-      location.pathname
-    );
-  }, [location.pathname]);
 
   return (
     <section className="bg-white">
